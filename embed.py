bfa41c8d02c52ebfb6bbd01b3eb47d2fcafa53c7
rushabh-v <vasanirushabh24@gmail.com>
2020-11-21 18:40:50 +0530

    fix bug of extra 'n' in embadded tweets

--- embed.py
+++ embed.py
@@ -8,8 +8,9 @@ def normalize_tweet_content(code):
 
 def get_html_content(json_content):
     start = json_content.index('html":"<blockquote') + 7
+    pre_n = end = json_content.index('</blockquote>') + 13
     end = json_content.index('</script>') + 9
-    return json_content[start:end]
+    return json_content[start:pre_n] + json_content[pre_n+1:end]
 
 def get_tweet_html(tweet_id):
     url = ("https://publish.twitter.com/oembed?"
